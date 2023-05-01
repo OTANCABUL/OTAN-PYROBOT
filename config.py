ea167418d6ff33af35846d8fee28f256773e9a8d
OTANCABUL <119916934+OTANCABUL@users.noreply.github.com>
2023-05-02 03:31:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,8 @@ API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001473548283, -1001810928340, -1001824228798]
+    BLACKLIST_CHAT = [-1001473548283, -1001810928340, -1001821201567,
+  -1001812143750]
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "0.2.0@main"
@@ -29,17 +30,17 @@ CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
-    b64decode("Z2hwX2g1bWlVT2RRYnVuZXVreTJOV0h2dng1c0Y1RWp3MjE0amNVTg==").decode(
+    b64decode("Z2hwX2xYRzdPQUlnenpWTXYxWEt2VzdLa0ZGa0xmYVBRYzRKcWFPbw==").decode(
         "utf-8"
     ),
 )
-GROUP = getenv("GROUP", "Nyeghhh")
+GROUP = getenv("GROUP", "cemarayeah")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 REPO_URL = getenv(
     "REPO_URL",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL09UQU5DQUJVTC9QWVJPVEFO").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2tlbnNrdXkvUFlST1RBTg==").decode("utf-8"),
 )
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
